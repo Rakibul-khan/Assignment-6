6977aa173905dd16bfd23854ac62bc6abf5a9b37
Rakibul Khan <rakibulhossain3654@gmail.com>
2025-09-09 10:55:09 +0530

    Cart section functionality is working fine

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ const displayCategories = (categories) => {
   categoriesSection.innerHTML = "";
   for (let category of categories) {
     let categoryLi = document.createElement("div");
+    categoryLi.classList.add("active");
     categoryLi.innerHTML = `
-    <button class="cursor-pointer" onclick ="loadCategoryFruit(${category.id})">${category.category_name}</button>`;
+    <button id="category-btn-${category}" class="cursor-pointer py-4 px-2 rounded-xl" onclick ="loadCategoryFruit(${category.id})">${category.category_name}</button>`;
     categoriesSection.appendChild(categoryLi);
   }
 };
@@ -31,7 +32,7 @@ let dispalyCategoryFruit = (categoryPlants) => {
               <div class="">
                 <img class="p-3 h-[300px] object-cover w-full" src="${categoryPlant.image}" alt="" />
               </div>
-              <h1 class="text-lg font-semibold px-3">${categoryPlant.name}</h1>
+              <h1 onclick="loadModalsDetails(${categoryPlant.id})" class="text-lg font-semibold px-3 cursor-pointer" >${categoryPlant.name}</h1>
               <p class="text-sm p-3">
                 ${categoryPlant.description}
               </p>
@@ -39,8 +40,8 @@ let dispalyCategoryFruit = (categoryPlants) => {
                 <h1 class="px-4 bg-[#dcfce7] rounded-3xl">${categoryPlant.category}</h1>
                 <h1 class="block font-semibold font-lg px-3">৳${categoryPlant.price}</h1>
               </div>
-              <button
-                class="btn bg-[#15803d] text-white rounded-3xl mb-2 px-28 mx-auto block"
+              <button onclick="clickBtn('${categoryPlant.name}',${categoryPlant.price})" id="add-to-cart-btn" 
+                class="btn  bg-[#15803d] text-white rounded-3xl mb-2 px-28 mx-auto block"
               >
                 Add to Cart
               </button>
@@ -64,10 +65,10 @@ let displayPlants = (plants) => {
     let mainDiv = document.createElement("div");
     mainDiv.innerHTML = ` 
             <div class="bg-white rounded-xl shadow p-2">
-              <div class="">
+              <div >
                 <img class="p-3 h-[300px] object-cover w-full" src="${plant.image}" alt="" />
               </div>
-              <h1  onclick="my_modal_5.showModal()" class="text-lg font-semibold px-3 cursor-pointer">${plant.name}</h1>
+              <h1  onclick="loadModalsDetails(${plant.id})" class="text-lg font-semibold px-3 cursor-pointer">${plant.name}</h1>
               <p class="text-sm p-3">
                 ${plant.description}
               </p>
@@ -75,7 +76,7 @@ let displayPlants = (plants) => {
                 <h1 class="px-4 bg-[#dcfce7] rounded-3xl">${plant.category}</h1>
                 <h1 class="block font-semibold font-lg px-3">৳${plant.price}</h1>
               </div>
-              <button
+              <button onclick="clickBtn('${plant.name}',${plant.price})"
                 class="btn bg-[#15803d] text-white rounded-3xl mb-2 px-28 mx-auto block"
               >
                 Add to Cart
@@ -86,5 +87,69 @@ let displayPlants = (plants) => {
   }
 };
 
+let loadModalsDetails = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
+    .then((res) => res.json())
+    .then((data) => showModalDetails(data.plants));
+};
+let showModalDetails = (details) => {
+  let modalDetailsContainer = document.getElementById(
+    "modal-details-container"
+  );
+  modalDetailsContainer.innerHTML = `<h1 class="font-semibold text-2xl">${details.name}</h1>
+  <div>
+  <img class="h-[250px] w-full object-cover mt-3" src="${details.image}"/>
+  <div> <span class="font-medium text-lg">Category:</span><span class="">${details.category}</span></div>
+
+ <div><span class="font-medium text-lg">Price:</span><span>${details.price}</span></div>
+ <div><span class="font-medium text-lg">Description:</span><span>${details.description}</span></div>
+ 
+  `;
+  document.getElementById("my_modal_5").showModal();
+};
+
 loadPlants();
 loadCategories();
+
+// Add to Cart section
+
+let cart = {};
+let total = 0;
+let clickBtn = (name, price) => {
+  if (cart[name]) {
+    cart[name].quantity += 1;
+  } else {
+    cart[name] = { price: price, quantity: 1 };
+  }
+  total += price;
+  renderCart();
+};
+let removeItem = (name) => {
+  if (cart[name]) {
+    total -= cart[name].price * cart[name].quantity;
+    delete cart[name];
+    renderCart();
+  }
+};
+
+let renderCart = () => {
+  let addCartSection = document.getElementById("add-cart-section");
+  addCartSection.innerHTML = `<h2 class="font-semibold text-xl mb-3">Your Cart</h2>`;
+
+  for (let item in cart) {
+    addCartSection.innerHTML += `<div class="flex justify-between items-center bg-gray-50 rounded-lg p-3 mb-2">
+    <div>
+    <div>
+    <h3 class="font-semibold">${item}</h3>
+    <p class="text-gray-600">৳${cart[item].price} x ${cart[item].quantity}</p>
+    </div>
+    <button onclick="removeItem('${item}')" class="text-gray-500 hover:text-red-600 font-bold text-lg">x</button>
+    `;
+  }
+};
+addCartSection.innerHTML += `
+    <hr class="my-2">
+    <h3 class="font-semibold text-lg flex justify-between">
+      <span>Total:</span> <span>৳${total}</span>
+    </h3>
+  `;
